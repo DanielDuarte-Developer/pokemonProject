7d206d7af06dc6c447d8ffef4dbba9bc922c5f9d
DanielDuarte-Developer <daniel.duarte@cmas-systems.com>
2023-11-20 17:06:07 +0000

    First Commit

---
+++ src/main/java/com/ips/tpsi/pokemowebapp/PokemowebappApplication.java
@@ -0,0 +1,13 @@
+package com.ips.tpsi.pokemowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemowebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemowebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ips/tpsi/pokemowebapp/PokemowebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ips.tpsi.pokemowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemowebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
