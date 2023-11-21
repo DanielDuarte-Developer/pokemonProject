d33ac4a15356951b9fb09452897f730b7ab6290c
DanielDuarte-Developer <daniel.duarte@cmas-systems.com>
2023-11-21 18:08:32 +0000

    Created 2 new fuctions with GetMapping

--- src/main/java/com/ips/tpsi/pokemowebapp/controller/WebController.java
+++ src/main/java/com/ips/tpsi/pokemowebapp/controller/WebController.java
@@ -2,11 +2,22 @@ package com.ips.tpsi.pokemowebapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WebController {
-    @GetMapping("/")
-    public String getHome(){
-        return "index";
+    @GetMapping({"/","/home"})
+    public ModelAndView getHome(){
+        ModelAndView mv = new ModelAndView("index");
+        // Aceder à business component > acede ao repositorio > obtem dados
+
+        return mv;
+    }
+    @GetMapping("/name")
+    public ModelAndView getName(){
+        ModelAndView mv = new ModelAndView("index");
+        // Aceder à business component > acede ao repositorio > obtem dados
+        mv.addObject("name","Daniel");
+        return mv;
     }
 }
